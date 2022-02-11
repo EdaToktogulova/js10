2b47ac43519f91ea5e2761c24276af6064eb4309
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-02-11 14:31:52 +0600

    news

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let example1 = document.querySelector('#example1');
 example1.addEventListener('click', function(event) {
+   window.location.href = "https://bing.com";
+
    event.preventDefault();
 
    //alert('Hello world');
@@ -7,5 +9,11 @@ example1.addEventListener('click', function(event) {
 
 let  example2 =  document.querySelector('#example2');
 example2.addEventListener('click', function(event) {
+
+   window.location.href = "https://codigniter.com";
    event.preventDefault();
+});
+
+document.body.addEventListener('contextmenu', function(event) {
+   
 });
