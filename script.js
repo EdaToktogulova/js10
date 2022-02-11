ad3459a1041f8b97041513ce56062ba6d9127d5f
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-02-11 14:56:36 +0600

    news

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let  example2 =  document.querySelector('#example2');
 example2.addEventListener('click', function(event) {
 
    window.location.href = "https://codigniter.com";
-   event.preventDefault();
+   //event.preventDefault();
 });
 
 document.body.addEventListener('contextmenu', function(event) {
@@ -19,6 +19,20 @@ document.body.addEventListener('contextmenu', function(event) {
 });
 
 document.querySelector('div').addEventListener('dblclick', function(event) {
-   console.log(event);
-   event.preventDefault();
+   alert('Hello');
+   //event.preventDefault(); -- doesn't work
 });
+
+let example = document.createElement('section');
+example.style.width = '20px';
+example.style.height = '20px';
+example.style.backgroundColor = 'black';
+example.style.position = 'absolute';
+document.body.append(example);
+
+
+document.body.addEventListener('div').addEventListener('mousemove', function(event) {
+   example.style.top = event.clientY + 'px';
+   example.style.top = event.clientX + 'px';
+});
+
